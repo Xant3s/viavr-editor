677685df5303fa5711250ed2b8fa0336f92ce113
Samuel Truman <samuel.truman@uni-wuerzburg.de>
2022-01-14 18:48:07 +0100

    Cleanup

--- code/src/App.ts
+++ code/src/App.ts
@@ -5,11 +5,3 @@ import Main from './main'
 
 Main.main(app, BrowserWindow)
 new HelloWorld()
-
-
-// const {BrowserView} = require('electron')
-// const win = new BrowserWindow({width: 800, height: 600})
-// const view = new BrowserView()
-// win.setBrowserView(view)
-// view.setBounds({x: 0, y: 0, width: 300, height: 300})
-// view.webContents.loadURL('https://github.com')

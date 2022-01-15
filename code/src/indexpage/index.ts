7cf4fd0deaa379f5a0e7aad51016c9f26c9952f7
Samuel Truman <samuel.truman@uni-wuerzburg.de>
2022-01-15 17:13:35 +0100

    Refactoring using jQuery

--- code/src/indexpage/index.ts
+++ code/src/indexpage/index.ts
@@ -1,5 +1,5 @@
 import {ipcRenderer as ipc} from 'electron'
-// import $ = require('jquery')
+import $ = require('jquery')
 
 try {
     require('electron-reloader')(module)
@@ -12,25 +12,7 @@ btnTest?.addEventListener('click', () => {
 
 
     const spoke = document.getElementById('iframe-spoke') as HTMLIFrameElement
-    const spokeDocument = spoke.contentDocument
-    // const spoke = $('#spoke-container')
+    const h1 = $(spoke).contents().find('h1')
+    h1.text('Hello World')
 
-    // @ts-ignore
-    spokeDocument?.getElementsByTagName('h1')[0].innerText = 'Hello World'
-
-    // const h1 = spoke.getElementsByTagName('h1')[0] as HTMLHeadingElement
-    // console.log(h1 === undefined)
-    // h1.innerText = 'Hello'
-
-    // $('#spoke-container').('h1').text('Hello')
-
-    // window.frames['iframe-spoke'].document.getElementsByTagName('h1')[0].innerText = 'Hello'
-
-    // query for h1 in spoke
-    // const h1 = spoke.contents().find('h1')
-    // @ts-ignore
-    // h1?.textContent = 'Hello World'
-
-    // console.log(h1 === null)
-    // log text in h1
 })

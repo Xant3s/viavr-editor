e96a9af1667e9edab57995e1c515c53bbde5f2e3
Samuel Truman <samuel.truman@uni-wuerzburg.de>
2022-01-23 14:34:06 +0100

    moved BuildDialog to class

--- code/src/Editor/BuildDialog/BuildDialog.ts
+++ code/src/Editor/BuildDialog/BuildDialog.ts
@@ -1,62 +1,68 @@
 import {ipcRenderer as ipc} from 'electron'
 
-const btnSelectUnity = document.getElementById('btn-select-unity-path')
-const btnConfirmUnityPath = document.getElementById('btn-confirm-unity-path')
-const btnQueryPackages = document.getElementById('btn-query-packages')
-const btnCreateProject = document.getElementById('btn-create-project')
-const btnBuildProject = document.getElementById('btn-build-project')
-const unityPathText = document.getElementById('unity-path') as HTMLInputElement
-const packageList = document.getElementById('package-list') as HTMLDivElement
+class BuildDialog {
+    readonly btnSelectUnity = document.getElementById('btn-select-unity-path')
+    readonly btnConfirmUnityPath = document.getElementById('btn-confirm-unity-path')
+    readonly btnQueryPackages = document.getElementById('btn-query-packages')
+    readonly btnCreateProject = document.getElementById('btn-create-project')
+    readonly btnBuildProject = document.getElementById('btn-build-project')
+    readonly unityPathText = document.getElementById('unity-path') as HTMLInputElement
+    readonly packageList = document.getElementById('package-list') as HTMLDivElement
+
+    constructor() {
+        this.btnConfirmUnityPath?.addEventListener('click', () => {
+            const unityPath = this.unityPathText?.value
+            ipc.send('set-unity-path', unityPath)
+            if(unityPath !== undefined)
+                (this.btnCreateProject as HTMLInputElement).disabled = false
+            console.log('SET UNITY PATH')
+        })
+
+        this.btnSelectUnity?.addEventListener('click', () => ipc.send('select-unity-path'))
+        this.btnQueryPackages?.addEventListener('click', () => ipc.send('query-available-packages'))
+        this.btnCreateProject?.addEventListener('click', () => ipc.send('create-unity-project', this.getSelectedPackages()))
+        this.btnBuildProject?.addEventListener('click', () => ipc.send('build-unity-project'))
 
 
-btnConfirmUnityPath?.addEventListener('click', () => {
-    const unityPath = unityPathText?.value
-    ipc.send('set-unity-path', unityPath)
-    if(unityPath !== undefined)
-        (btnCreateProject as HTMLInputElement).disabled = false
-    console.log('SET UNITY PATH')
-})
+        ipc.on('selected-unity-path', (_, unityPath) => this.unityPathText.value = unityPath ?? this.unityPathText.value)
 
-btnSelectUnity?.addEventListener('click', () => ipc.send('select-unity-path'))
-btnQueryPackages?.addEventListener('click', () => ipc.send('query-available-packages'))
-btnCreateProject?.addEventListener('click', () => ipc.send('create-unity-project', getSelectedPackages()))
-btnBuildProject?.addEventListener('click', () => ipc.send('build-unity-project'))
+        ipc.on('clear-packages', () => this.packageList.innerHTML = '')
 
-ipc.on('selected-unity-path', (_, unityPath) => unityPathText.value = unityPath ?? unityPathText.value)
+        ipc.on('add-package', (_, p) => {
+            const label = document.createElement('label')
+            const description = document.createTextNode(p.displayName)
+            const versionContainer = document.createElement('span')
+            const checkbox = document.createElement('input')
+            const br = document.createElement('br')
+            checkbox.type = 'checkbox'
+            checkbox.title = label.title = p.description
+            checkbox.classList.add('package-select-checkbox')
+            checkbox.id = p.name
+            versionContainer.style.color = 'gray'
+            versionContainer.appendChild(document.createTextNode(' ' + p.version))
+            label.appendChild(checkbox)
+            label.appendChild(description)
+            label.appendChild(versionContainer)
+            label.appendChild(br)
+            this.packageList.appendChild(label)
 
-ipc.on('clear-packages', () => packageList.innerHTML = '')
+            if(p.mandatory) {
+                checkbox.checked = true
+                checkbox.disabled = true
+            }
+        })
 
-ipc.on('add-package', (_, p) => {
-    const label = document.createElement('label')
-    const description = document.createTextNode(p.displayName)
-    const versionContainer = document.createElement('span')
-    const checkbox = document.createElement('input')
-    const br = document.createElement('br')
-    checkbox.type = 'checkbox'
-    checkbox.title = label.title = p.description
-    checkbox.classList.add('package-select-checkbox')
-    checkbox.id = p.name
-    versionContainer.style.color = 'gray'
-    versionContainer.appendChild(document.createTextNode(' ' + p.version))
-    label.appendChild(checkbox)
-    label.appendChild(description)
-    label.appendChild(versionContainer)
-    label.appendChild(br)
-    packageList.appendChild(label)
-
-    if(p.mandatory) {
-        checkbox.checked = true
-        checkbox.disabled = true
+        ipc.on('ready-to-build-project', () =>
+            (this.btnBuildProject as HTMLInputElement).disabled = false)
     }
-})
 
-function getSelectedPackages() {
-    const checkboxes = Array.from(document.getElementsByClassName('package-select-checkbox'))
-    const packages = new Map<string, boolean>()
-    checkboxes.forEach(checkbox => packages.set(checkbox.id, (checkbox as HTMLInputElement).checked))
-    return packages
+    private getSelectedPackages() {
+        const checkboxes = Array.from(document.getElementsByClassName('package-select-checkbox'))
+        const packages = new Map<string, boolean>()
+        checkboxes.forEach(checkbox => packages.set(checkbox.id, (checkbox as HTMLInputElement).checked))
+        return packages
+    }
+
 }
 
-ipc.on('ready-to-build-project', () => {
-    (btnBuildProject as HTMLInputElement).disabled = false
-})
+new BuildDialog()

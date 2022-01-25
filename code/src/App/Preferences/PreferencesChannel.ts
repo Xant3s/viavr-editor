1f9553314a518fc7b24362e22ebc1beb922cdb88
Samuel Truman <samuel.truman@uni-wuerzburg.de>
2022-01-25 16:54:45 +0100

    Load initial preferences in UI

--- code/src/App/Preferences/PreferencesChannel.ts
+++ code/src/App/Preferences/PreferencesChannel.ts
@@ -1,15 +1,18 @@
 import {IpcMainEvent} from 'electron'
 import {IpcChannel, IpcRequest} from '../IpcChannel'
+import PreferencesManager from './PreferencesManager'
 
 export default class PreferencesChannel extends IpcChannel {
     getName(): string {
-        return 'preferences-info';
+        return 'preferences-request';
     }
 
     handle(event: IpcMainEvent, request: IpcRequest) {
         if(!request.responseChannel) {
-            request.responseChannel = `${this.getName}-response`
+            request.responseChannel = `preferences-response`
         }
-        event.sender.send(request.responseChannel, 'test')
+        if(request.params) {
+            event.sender.send(request.responseChannel, PreferencesManager.getInstance().get<string>(request.params[0]))
+        }
     }
 }
--- code/src/Editor/Preferences/Preferences.ts
+++ code/src/Editor/Preferences/Preferences.ts
@@ -2,23 +2,39 @@ import {ipcRenderer as ipc} from 'electron'
 import $ = require('jquery')
 import {IpcService} from '../IpcService'
 
-const ipcs = new IpcService()
+const ipcService = new IpcService()
 
 class Preferences {
     constructor() {
+        this.loadInitialValues()
+
         this.addUpdatePreferencesEvent<string>($('#unity-path'), 'unityPath')
         this.addUpdatePreferencesEvent<string>($('#package-registry-name'), 'packageRegistryName')
         this.addUpdatePreferencesEvent<string>($('#package-registry-url'), 'packageRegistryUrl')
         this.addUpdatePreferencesEvent<string>($('#package-registry-scope'), 'packageRegistryScope')
 
         $('#btn-select-unity-path').on('click', async () => {
-            const res = await ipcs.send<string>('preferences-info')
-            console.log(res)
         })
     }
 
+    private loadInitialValues() {
+        this.loadPreference($('#unity-path'), 'unityPath')
+        this.loadPreference($('#package-registry-name'), 'packageRegistryName')
+        this.loadPreference($('#package-registry-url'), 'packageRegistryUrl')
+        this.loadPreference($('#package-registry-scope'), 'packageRegistryScope')
+    }
+
+    private async loadPreference(queryElement: JQuery<HTMLElement>, name: string) {
+        const data = await ipcService.send<string>('preferences-request',
+            {
+                responseChannel: `preferences-response-${name}`,
+                params: [name]
+            })
+        queryElement.val(data)
+    }
+
     private addUpdatePreferencesEvent<Type>(queryElement: JQuery<HTMLElement>, name: string = 'packageRegistryName') {
-        queryElement.on('change', () => {
+        queryElement.on('change', async () => {
             ipc.send('preferences-changed', {
                 name: name,
                 value: queryElement.val()

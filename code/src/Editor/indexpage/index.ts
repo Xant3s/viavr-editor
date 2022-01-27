5742730a11b293da91b721ba3cb9a384ab23bf67
Samuel Truman <samuel.truman@uni-wuerzburg.de>
2022-01-27 22:12:50 +0100

    Added test button to export scene as .glb

--- code/src/Editor/indexpage/index.ts
+++ code/src/Editor/indexpage/index.ts
@@ -1,7 +1,17 @@
 // import {ipcRenderer as ipc} from 'electron'
-// import $ = require('jquery')
+import $ = require('jquery')
 
 // Hot reloading
 try {
     require('electron-reloader')(module)
 } catch (_) {}
+
+const $spoke = $('#iframe-spoke').contents()
+const $$ = (query) => $spoke.find(query)
+
+
+$('#btn-test').on('click', () => {
+    const $exportGlb = $$('#app nav nav nav div:eq(5)')
+    $exportGlb.hide()
+    $exportGlb.trigger('click')
+})

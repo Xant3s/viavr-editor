367d3501eb47f949d663b3cfc7cf5a35929b45f4
Samuel Truman <samuel.truman@uni-wuerzburg.de>
2022-02-03 01:40:45 +0100

    Added dummy test

---
+++ code/test/BuildSystem/UnityBuildManager.spec.ts
@@ -0,0 +1,12 @@
+// import UnityPackageManager from '../../src/App/BuildSystem/UnityPackageManager'
+import {describe, it} from 'mocha'
+import {expect} from 'chai'
+
+describe('UnityBuildManager', () => {
+    describe('Test addScopedRegistryToManifest', () => {
+        it('should create a array of scoped registries if there is none', () => {
+            // const UnityBuildManager = new UnityBuildManager(null)
+            expect(1).to.equal(1)
+        })
+    })
+})

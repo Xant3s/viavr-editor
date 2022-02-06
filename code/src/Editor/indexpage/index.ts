d04eded88f1c0b882ed68ccbc6b16e92dbefc66f
Samuel Truman <samuel.truman@uni-wuerzburg.de>
2022-02-06 15:19:24 +0100

    Removed test button

--- code/src/Editor/indexpage/index.ts
+++ code/src/Editor/indexpage/index.ts
@@ -7,21 +7,9 @@ try {
 } catch (_) {}
 
 
-// const $spoke = $('#iframe-spoke').contents()
-// const $$ = (query) => $spoke.find(query)
-
 
 
 ipc.on('spoke:export-scene', () => {
-    const $spoke = $('#iframe-spoke').contents()
-    const $$ = (query) => $spoke.find(query)
-
-    const $exportGlb = $$('#app nav nav nav div:eq(5)')
-    $exportGlb.hide()
-    $exportGlb.trigger('click')
-})
-
-$('#btn-test').on('click', () => {
     const $spoke = $('#iframe-spoke').contents()
     const $$ = (query) => $spoke.find(query)
 

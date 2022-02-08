ae1477fb226746e1f658f90803806d1194741aed
Samuel Truman <samuel.truman@uni-wuerzburg.de>
2022-02-08 17:50:43 +0100

    Automatically handles the export dialog

--- code/src/Editor/indexpage/index.ts
+++ code/src/Editor/indexpage/index.ts
@@ -16,5 +16,21 @@ ipc.on('spoke:export-scene', () => {
     // const $exportGlb = $$('#app nav nav nav div:eq(5)')
     const $exportGlb = $$('div:contains("Export as binary glTF"):last')
     $exportGlb.hide()
+    setInterval(foo, 100)
     $exportGlb.trigger('click')
 })
+
+const foo = () => {
+    const $spoke = $('#iframe-spoke').contents()
+    const $$ = (query) => $spoke.find(query)
+
+    const $exportProjectBtn = $$('button:contains("Export Project")')
+    // console.log($exportProjectBtn === undefined)
+    // console.log($exportProjectBtn.length)
+    if($exportProjectBtn.length > 0) {
+        // const $form = $exportProjectBtn.closest('form')
+        // $form.hide()
+        $exportProjectBtn.last().trigger('click')
+    }
+}
+// setInterval(foo, 500)

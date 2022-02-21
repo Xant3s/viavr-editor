a7bdb4984caeceb7033e1c127f776799d7727b12
Samuel Truman <samuel.truman@uni-wuerzburg.de>
2022-02-21 20:15:36 +0100

    Added Unity Bridge

---
+++ code/src/App/BuildSystem/UnityBridge.ts
@@ -0,0 +1,23 @@
+import PreferencesManager from '../Preferences/PreferencesManager'
+import util from 'util'
+
+const exec = util.promisify(require('child_process').exec)
+
+
+export default class UnityBridge {
+    public async build(projectPath: string) {
+        await this.invokeUnityMethod('de.jmu.ge.viavr.UnityBridge.Core.UnityBridge.ExecuteAll', projectPath)
+    }
+
+    private async invokeUnityMethod(method: string, projectPath: string) {
+        const unityPath = PreferencesManager.getInstance().get<string>('unityPath')
+        const unityAppPath = UnityBridge.isMacOS()? `${unityPath}/Contents/MacOS/Unity` : `${unityPath}`
+        const command = `"${unityAppPath}" -quit -batchmode -projectPath "${projectPath}" -executeMethod ${method}`
+        const {stdout, stderr } = await exec(command)
+        if(stderr) console.log(stderr)
+        console.log(stdout)
+    }
+
+    private static isMacOS = () => process.platform === 'darwin';
+
+}

b5bd800ed7c016a860f0df8cf3ed8cdfd284a64e
Samuel Truman <samuel.truman@uni-wuerzburg.de>
2022-02-28 13:47:45 +0100

    Refactoring

--- code/src/Editor/indexpage/index.ts
+++ code/src/Editor/indexpage/index.ts
@@ -1,4 +1,5 @@
 import {ipcRenderer as ipc} from 'electron'
+import {SceneExport} from '../SpokeEditor/SceneExport'
 import $ = require('jquery')
 
 // Hot reloading
@@ -8,10 +9,6 @@ try {
 }
 
 
-const $$ = (query: string) => {
-    const $spoke = $('#iframe-spoke').contents()
-    return $spoke.find(query)
-}
 
 $('#create-new-project-btn').on('click', () => {
     ipc.send('project-manager:create-new-project')
@@ -33,43 +30,5 @@ function onProjectSelected() {
     $('#spoke-container').show()
 }
 
-
-class SceneExport {
-    constructor() {
-        ipc.on('spoke:export-scene', async() => {
-            const exportSceneButton = $$('div:contains("Export as binary glTF"):last')
-            exportSceneButton.hide()
-            exportSceneButton.trigger('click')
-            const exportButton = await this.htmlElement('button:contains("Export Project"):last')
-            await this.handleExportOptionsDialog(exportButton)
-        })
-    }
-
-    private async handleExportOptionsDialog(exportProjectBtn) {
-        exportProjectBtn.closest('form').hide()
-        exportProjectBtn.trigger('click')
-        const dialogTitle = await this.htmlElement('span:contains("Exporting Project"):last')
-        dialogTitle.text('Exporting Scene...')
-        $$('div:contains("project"):last').text('Exporting scene...')
-    }
-
-    private htmlElement(query: string, updateIntervalInMs: number = 100, timeout: number = 10_000): Promise<JQuery<HTMLElement>> {
-        return new Promise((resolve, reject) => {
-            const checkIfAvailable = () => {
-                const element = $$(query)
-                if(element.length > 0) {
-                    clearInterval(intervalId)
-                    resolve(element.first())
-                }
-            }
-
-            const intervalId = setInterval(checkIfAvailable, updateIntervalInMs)
-            setTimeout(() => {
-                clearInterval(intervalId)
-                reject(new Error('Timeout'))
-            }, timeout)
-        })
-    }
-}
 
 new SceneExport()

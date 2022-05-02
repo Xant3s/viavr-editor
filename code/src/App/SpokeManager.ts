c0ecb79f9395e43beaf24e319b8d32937aaea2e6
Samuel Truman <samuel.truman@uni-wuerzburg.de>
2022-05-02 17:46:56 +0200

    Stop spoke is now optional in dev if app is not packaged

--- code/src/App/SpokeManager.ts
+++ code/src/App/SpokeManager.ts
@@ -1,12 +1,13 @@
-import {ipcMain as ipc} from 'electron'
+import {app, ipcMain as ipc} from 'electron'
 import * as child_process from 'child_process'
 import kill from 'tree-kill'
 import AppUtils from './AppUtils'
+import PreferencesManager from './Preferences/PreferencesManager'
 
 export default class SpokeManager {
     private static instance: SpokeManager
     private startCommand: string = `cd ${AppUtils.getResPath()}/plugins/Spoke && yarn start`
-    private spoke: child_process.ChildProcess
+    private spoke!: child_process.ChildProcess
 
 
     public static getInstance(): SpokeManager {
@@ -17,16 +18,22 @@ export default class SpokeManager {
     }
 
     private constructor() {
-        // if(app.isPackaged){
+        this.startSpoke()
+        ipc.on('app-quit', this.stopSpoke)
+    }
+
+    private startSpoke() {
         this.spoke = child_process.spawn(this.startCommand, [], {
             shell: true,
             detached: true,
         })
-        ipc.on('app-quit', this.stopSpoke)
-        // }
     }
 
     private stopSpoke() {
-        kill(SpokeManager.getInstance().spoke.pid)
+        const shouldStop: boolean = PreferencesManager.getInstance().get<boolean>('dev-stopSpoke') as boolean
+
+        if(app.isPackaged || shouldStop) {
+            kill(SpokeManager.getInstance().spoke.pid)
+        }
     }
 }

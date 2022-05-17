a43662dbbbccdb33a2082a177aac53330f1ac72a
Samuel Truman <samuel.truman@uni-wuerzburg.de>
2022-05-17 14:57:29 +0200

    Init react + electron + typescript
    # Conflicts:
    #	.gitignore
    #	README.md

---
+++ code/src/App.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+// import {ipcRenderer} from 'electron'
+
+const App = () => {
+  const foo = () => {
+    console.log('foo')
+    // ipcRenderer.send('foo')
+    return 'foo'
+  }
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <p>{foo()}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;

e7f8df057a12304a0fafc18a7a677b87dbca25ce
Samuel Truman <samuel.truman@uni-wuerzburg.de>
2022-05-18 20:14:08 +0200

    Added build dialog component

--- code/frontend/src/App.tsx
+++ code/frontend/src/App.tsx
@@ -1,18 +1,19 @@
 import './App.css';
 import {HashRouter as Router, Route, Routes} from 'react-router-dom'
 import {HelloWorld} from './Components/HelloWorld'
-import {Preferences } from './Components/Preferences';
+import {Preferences} from './Components/Preferences';
+import {BuildDialog} from './Components/BuildDialog';
 
 const App = () => {
-
-  return (
-      <Router>
-        <Routes>
-          <Route path='/' element={<HelloWorld/>}/>
-          <Route path='/preferences' element={<Preferences/>}/>
-        </Routes>
-      </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path='/' element={<HelloWorld/>}/>
+                <Route path='/preferences' element={<Preferences/>}/>
+                <Route path='/build-dialog' element={<BuildDialog/>}/>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;

71e2935aaba7f385a5756491402041a290a29637
Samuel Truman <samuel.truman@uni-wuerzburg.de>
2022-05-27 19:21:48 +0200

    Moved API to dedicated file

--- code/backend/API.ts
+++ code/backend/API.ts
@@ -1,3 +1,6 @@
+import {ipcRenderer} from 'electron'
+import Path from 'path'
+
 export const channels = {
     "toMain": {
         selectUnityPath: 'BuildSystem:select-unity-path',
@@ -23,4 +26,52 @@ export const channels = {
         projectOpened: "project-manager:project-opened",
         spokeExportScene: "spoke:export-scene"
     }
+}
+
+class ValidChannels {
+    toMain: string[]
+    fromMain: string[]
+
+    constructor() {
+        this.toMain = Object.values(channels.toMain)
+        this.fromMain = Object.values(channels.fromMain)
+    }
+}
+
+const send = (channel: string, ...args: any) => {
+    const validChannels = new ValidChannels()
+    if(validChannels.toMain.includes(channel)) {
+        ipcRenderer.send(channel, ...args)
+    } else {
+        console.error(`Invalid channel: ${channel}`)
+    }
+}
+
+const invoke = async (channel: string, ...args: any) => {
+    const validChannels = new ValidChannels()
+    if(validChannels.toMain.includes(channel)) {
+        return ipcRenderer.invoke(channel, ...args)
+    } else {
+        console.error(`Invalid channel: ${channel}`)
+    }
+    return new Promise((_, reject) => { reject("Invalid channel") })
+}
+
+//@ts-ignore
+const on = (channel: string, func) => {
+    const validChannels = new ValidChannels()
+    if(validChannels.fromMain.includes(channel)) {
+        // Deliberately strip event as it includes `sender`
+        ipcRenderer.on(channel, (event, ...args) => func(...args))
+    } else {
+        console.error(`Invalid channel: ${channel}`)
+    }
+}
+
+export const API = {
+    send: send,
+    invoke: invoke,
+    on: on,
+    Path: Path,
+    channels: channels
 }

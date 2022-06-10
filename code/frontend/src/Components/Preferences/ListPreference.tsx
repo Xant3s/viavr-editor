9ef63e8ba26e98aac33e41798bd56acc26a7f13e
Samuel Truman <samuel.truman@uni-wuerzburg.de>
2022-06-10 17:32:20 +0200

    Fixed missing key warning

--- code/frontend/src/Components/Preferences/ListPreference.tsx
+++ code/frontend/src/Components/Preferences/ListPreference.tsx
@@ -4,19 +4,19 @@ export const ListPreference = ({id, label, value, onChange, createPrefComponent}
     return (
         <>
             <h5>{label}</h5>
-                    {
-                        value.map(entry => (
-                            <div className={'preference-list-entry'}>
-                                <div>{
-                                    Object.entries(entry)
-                                          .map(entry => createPrefComponent(entry[0], entry[1]))
-                                }</div>
-                                <div style={{display: 'flex', alignItems: 'center'}}>
-                                    <FaTimes onClick={() => {}} style={{color: 'black', cursor: 'pointer', marginLeft: 20}}/>
-                                </div>
-                            </div>
-                        ))
-                    }
+            {
+                value.map((item, index) => (
+                    <div className={'preference-list-entry'} key={index}>
+                        <div>{
+                            Object.entries(item)
+                                  .map(entry => createPrefComponent(entry[0], entry[1]))
+                        }</div>
+                        <div style={{display: 'flex', alignItems: 'center'}}>
+                            <FaTimes onClick={() => {}} style={{color: 'black', cursor: 'pointer', marginLeft: 20}}/>
+                        </div>
+                    </div>
+                ))
+            }
 
             <button id={`btn-add-${id}`}>Add</button>
         </>

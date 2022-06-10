e86c232c4a30269293e6ffdfb49934d4111f2cac
Samuel Truman <samuel.truman@uni-wuerzburg.de>
2022-06-10 15:00:10 +0200

    WIP display multiple registries

--- code/frontend/src/Components/Preferences/ListPreference.tsx
+++ code/frontend/src/Components/Preferences/ListPreference.tsx
@@ -2,15 +2,17 @@ export const ListPreference = ({id, label, value, onChange, createPrefComponent}
     return (
         <>
             <h5>{label}</h5>
-            <div style={{flexDirection: 'row', display: 'flex'}}>
-                <div>
                     {
-                        value.map(entry => Object.entries(entry)
-                                                 .map(entry => createPrefComponent(entry[0], entry[1])))
+                        value.map(entry => (
+                            <div style={{flexDirection: 'row', display: 'flex', marginBottom: 10}}>
+                                <div>{
+                                    Object.entries(entry)
+                                          .map(entry => createPrefComponent(entry[0], entry[1]))
+                                }</div>
+                                <button id={`btn-remove-${id}`} style={{marginLeft: 10}}>Remove</button>
+                            </div>
+                        ))
                     }
-                </div>
-                <button id={`btn-remove-${id}`} style={{marginLeft: 10}}>Remove</button>
-            </div>
 
             <button id={`btn-add-${id}`}>Add</button>
         </>

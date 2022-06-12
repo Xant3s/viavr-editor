b828d80aa5c7bdb3c6777771580dd08d899d0900
Samuel Truman <samuel.truman@uni-wuerzburg.de>
2022-06-12 20:00:13 +0200

    Refactoring

--- code/frontend/src/Components/Preferences/Preferences.tsx
+++ code/frontend/src/Components/Preferences/Preferences.tsx
@@ -1,93 +1,15 @@
-import {FC, useEffect, useState} from 'react'
-import {Preference} from './Preference'
-import { StyledPreferences, PreferencesContainer } from '../StyledComponents/Preferences/StyledPreferences'
+import {FC} from 'react'
+import {Settings} from './Settings'
 
 export const Preferences: FC = () => {
-    const [prefs, setPrefs] = useState<Map<string, any>>(new Map())
-
-    const setPref = (key: string, value: any) => {
-        setPrefs(new Map(prefs.set(key, value)))
-    }
-
-    const loadPreferences = () => {
-        return api.invoke(api.channels.toMain.requestPreferences)
-    }
-
-    const updatePreference = (event, name: string) => {
-        const isComplex = prefs.get(name)['kind'] !== undefined
-        let newValue = isComplex ? {...prefs.get(name), value: event.target.value} : event.target.value
-        setPref(name, newValue)
-        api.send(api.channels.toMain.changePreference, {name: name, value: newValue})
-    }
-
-    const updateListPreference = (parentName: string, index: number, prefName: string, event) => {
-        if(prefs.size === 0) return
-        let newPrefs = new Map(prefs)
-        let list = newPrefs.get(parentName)['value']
-        if(list === undefined) return
-        list[index][prefName]['value'] = event.target.value
-        newPrefs.set(parentName, {...newPrefs.get(parentName), value: list})
-        setPrefs(newPrefs)
-        api.send(api.channels.toMain.changePreference, {name: parentName, value: newPrefs.get(parentName)})
-    }
-
-    const selectPath = async (prefName) => {
-        const path = await api.invoke(api.channels.toMain.showOpenFileDialog) as string
-        if(path === undefined) return
-        const newValue = {...prefs.get(prefName), value: path}
-        setPref(prefName, newValue)
-        api.send(api.channels.toMain.changePreference, {name: prefName, value: newValue})
-    }
-
-    const createPreferenceComponent = (prefKey: string) => {
-        const pref = prefs.get(prefKey)
-        return createPreferenceComponent2(prefKey, pref)
-    }
-
-    const createPreferenceComponent2 = (prefKey: string, pref: any, parentKey = '', index = -1) => {
-        const emptyList: string[] = []
-        const kind = pref['kind'] || 'string'
-        const label = pref['label'] || prefKey
-        const options = pref['options'] || emptyList
-        const value = pref['value'] || pref
-        const onChange = (e) => {
-            if(index !== -1) {
-                updateListPreference(parentKey, index, prefKey, e)
-            } else{
-                updatePreference(e, prefKey)
-            }
-        }
-        return (
-            <Preference id={prefKey} key={prefKey} label={label} value={value} onChange={onChange}
-                        kind={kind} options={options} selectPath={() => selectPath(prefKey)}
-                        createPrefComponent={createPreferenceComponent2} />
-        )
-    }
-
-    useEffect(() => {
-        const loadInitialValues = async() => {
-            const allPreferences = await loadPreferences() as [string, unknown][]
-            const preferencesExcludingDevPreferences = allPreferences.filter(pref => !pref[0].startsWith('dev-'))
-            preferencesExcludingDevPreferences.forEach(pref => setPref(pref[0] as string, pref[1]))
-        }
-
-        api.on(api.channels.fromMain.preferenceChangedFromBackendUnityPath, (data) => {setPref('unityPath', data)})
-
-        loadInitialValues()
-    })
-
-
     return (
-        <StyledPreferences>
-            <h1>Preferences</h1>
-            <br />
-
-            <PreferencesContainer>
-            {
-                Array.from(prefs.keys())
-                     .map(prefKey => createPreferenceComponent(prefKey))
-            }
-            </PreferencesContainer>
-        </StyledPreferences>
+        <Settings title={'Preferences'}
+                  loadSettingsChannel={api.channels.toMain.requestPreferences}
+                  changeSettingChannel={api.channels.toMain.changePreference}
+                  registerUpdateCallbacksFromBackend={(setPref) => {
+                      api.on(api.channels.fromMain.preferenceChangedFromBackendUnityPath, (data) => {
+                          setPref('unityPath', data)
+                      })
+                  }}/>
     )
 }
--- code/frontend/src/Components/ProjectSettings/ProjectSettings.tsx
+++ code/frontend/src/Components/ProjectSettings/ProjectSettings.tsx
@@ -1,93 +1,11 @@
-import {FC, useEffect, useState} from 'react'
-import {PreferencesContainer, StyledPreferences} from '../StyledComponents/Preferences/StyledPreferences'
-import {Preference} from '../Preferences/Preference'
+import {FC} from 'react'
+import {Settings} from '../Preferences/Settings'
+
 
 export const ProjectSettings: FC = () => {
-    const [settings, setSettings] = useState<Map<string, any>>(new Map())
-
-    const setPref = (key: string, value: any) => {
-        setSettings(new Map(settings.set(key, value)))
-    }
-
-    const loadSettings = () => {
-        return api.invoke(api.channels.toMain.requestProjectSettings)
-    }
-
-    const updatePreference = (event, name: string) => {
-        const isComplex = settings.get(name)['kind'] !== undefined
-        let newValue = isComplex ? {...settings.get(name), value: event.target.value} : event.target.value
-        setPref(name, newValue)
-        api.send(api.channels.toMain.changeProjectSetting, {name: name, value: newValue})
-    }
-
-    const updateListPreference = (parentName: string, index: number, prefName: string, event) => {
-        if(settings.size === 0) return
-        let newPrefs = new Map(settings)
-        let list = newPrefs.get(parentName)['value']
-        if(list === undefined) return
-        list[index][prefName]['value'] = event.target.value
-        newPrefs.set(parentName, {...newPrefs.get(parentName), value: list})
-        setSettings(newPrefs)
-        api.send(api.channels.toMain.changeProjectSetting, {name: parentName, value: newPrefs.get(parentName)})
-    }
-
-    const selectPath = async (prefName) => {
-        const path = await api.invoke(api.channels.toMain.showOpenFileDialog) as string
-        if(path === undefined) return
-        const newValue = {...settings.get(prefName), value: path}
-        setPref(prefName, newValue)
-        api.send(api.channels.toMain.changeProjectSetting, {name: prefName, value: newValue})
-    }
-
-    const createPreferenceComponent = (prefKey: string) => {
-        const pref = settings.get(prefKey)
-        return createPreferenceComponent2(prefKey, pref)
-    }
-
-    const createPreferenceComponent2 = (prefKey: string, pref: any, parentKey = '', index = -1) => {
-        const emptyList: string[] = []
-        const kind = pref['kind'] || 'string'
-        const label = pref['label'] || prefKey
-        const options = pref['options'] || emptyList
-        const value = pref['value'] || pref
-        const onChange = (e) => {
-            if(index !== -1) {
-                updateListPreference(parentKey, index, prefKey, e)
-            } else{
-                updatePreference(e, prefKey)
-            }
-        }
-        return (
-            <Preference id={prefKey} key={prefKey} label={label} value={value} onChange={onChange}
-                        kind={kind} options={options} selectPath={() => selectPath(prefKey)}
-                        createPrefComponent={createPreferenceComponent2} />
-        )
-    }
-
-    useEffect(() => {
-        const loadInitialValues = async() => {
-            const allPreferences = await loadSettings() as [string, unknown][]
-            const preferencesExcludingDevPreferences = allPreferences.filter(pref => !pref[0].startsWith('dev-'))
-            preferencesExcludingDevPreferences.forEach(pref => setPref(pref[0] as string, pref[1]))
-        }
-
-        loadInitialValues()
-    })
-
-
     return (
-        <>
-            <StyledPreferences>
-                <h1>Project Settings</h1>
-                <br />
-
-                <PreferencesContainer>
-                    {
-                        Array.from(settings.keys())
-                             .map(prefKey => createPreferenceComponent(prefKey))
-                    }
-                </PreferencesContainer>
-            </StyledPreferences>
-        </>
+        <Settings title={'Project Settings'}
+                  loadSettingsChannel={api.channels.toMain.requestProjectSettings}
+                  changeSettingChannel={api.channels.toMain.changeProjectSetting}/>
     )
 }

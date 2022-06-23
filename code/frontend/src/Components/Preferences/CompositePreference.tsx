deda8e3a5feac4a1edecd4ac24a8c143de4aad0d
Samuel Truman <samuel.truman@uni-wuerzburg.de>
2022-06-23 17:25:52 +0200

    Fixed composite display

--- code/frontend/src/Components/Preferences/CompositePreference.tsx
+++ code/frontend/src/Components/Preferences/CompositePreference.tsx
@@ -1,19 +1,25 @@
 import {PreferenceListEntry} from '../StyledComponents/Preferences/StyledPreferences'
 
 export const CompositePreference = ({id, label, value, onChange, createPrefComponent}) => {
+    const drawEntry = (entry) => {
+        return (
+            <div>
+                {createPrefComponent(entry[0], entry[1])}
+            </div>
+        )
+    }
+
     return (
         <>
             {label && <h5>{label}</h5>}
-            {
-                <PreferenceListEntry>
-                    <div>
-                        {
-                            Object.entries(value)
-                                  .map(entry => createPrefComponent(entry[0], entry[1]))
-                        }
-                    </div>
-                </PreferenceListEntry>
-            }
+            <PreferenceListEntry>
+                <div>
+                    {
+                        Object.entries(value)
+                              .map(entry => drawEntry(entry))
+                    }
+                </div>
+            </PreferenceListEntry>
         </>
     )
 }

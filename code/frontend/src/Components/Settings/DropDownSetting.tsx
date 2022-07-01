d9b45847ac2e76ee0a09c71570484595eeea9e2b
Samuel Truman <samuel.truman@uni-wuerzburg.de>
2022-07-01 18:10:41 +0200

    Improved dropdown setting

--- code/frontend/src/Components/Settings/DropDownSetting.tsx
+++ code/frontend/src/Components/Settings/DropDownSetting.tsx
@@ -1,16 +1,17 @@
+import {Select} from 'evergreen-ui'
 import {SettingEntryLabel} from '../StyledComponents/Preferences/StyledSettings'
 
 export const DropDownSetting = ({id, uuid, label, value, onChange, options}) => {
     return (
         <>
             {label && <SettingEntryLabel htmlFor={id}>{label}:</SettingEntryLabel>}
-            <select id={id} name={id} value={value} onChange={(e) => onChange(uuid, e.target.value)}>
+            <Select id={id} name={id} value={value} height={24} onChange={(e) => onChange(uuid, e.target.value)}>
                 {
                     options.map((option) => {
                         return <option key={option} value={option}>{option}</option>
                     })
                 }
-            </select>
+            </Select>
         </>
     )
 }

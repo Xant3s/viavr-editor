f40dacca2f43c8288a87c55019ec3291dac05cfb
Samuel Truman <samuel.truman@uni-wuerzburg.de>
2022-07-02 19:37:00 +0200

    Fixed page not loading in standalone mode

--- code/backend/Utils/ElectronUtils.ts
+++ code/backend/Utils/ElectronUtils.ts
@@ -1,9 +1,3 @@
-import * as isDev from 'electron-is-dev'
-
 export const loadPage = (window: Electron.BrowserWindow, page: string) => {
-    // if(isDev) {
-    //     window.loadURL(`http://localhost:3000#/${page}`)
-    // } else {
-        window.loadURL(`file://${__dirname}/../../index.html#/${page}`)
-    // }
+    window.loadURL(`file://${__dirname}/../../index.html#/${page}`)
 }

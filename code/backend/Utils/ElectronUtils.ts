ffaff686dd28ad60ca26ebccdb9a3e2f9ac580ce
Samuel Truman <samuel.truman@uni-wuerzburg.de>
2022-07-02 19:25:58 +0200

    Fixed file path

--- code/backend/Utils/ElectronUtils.ts
+++ code/backend/Utils/ElectronUtils.ts
@@ -1,9 +1,9 @@
 import * as isDev from 'electron-is-dev'
 
 export const loadPage = (window: Electron.BrowserWindow, page: string) => {
-    if(isDev) {
-        window.loadURL(`http://localhost:3000#/${page}`)
-    } else {
-        window.loadURL(`file://${__dirname}/../index.html#/${page}`)
-    }
+    // if(isDev) {
+    //     window.loadURL(`http://localhost:3000#/${page}`)
+    // } else {
+        window.loadURL(`file://${__dirname}/../../index.html#/${page}`)
+    // }
 }

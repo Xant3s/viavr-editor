6efbfe97eaa15625e5f1bd7dbf99c6ad1e6fe4ef
Samuel Truman <samuel.truman@uni-wuerzburg.de>
2022-07-20 14:25:15 +0200

    Fixed visual artifact in for large settings windows

--- code/frontend/src/Components/StyledComponents/Preferences/StyledSettings.ts
+++ code/frontend/src/Components/StyledComponents/Preferences/StyledSettings.ts
@@ -5,7 +5,7 @@ export const StyledSettings = styled.div`
   background-color: #15171b;
   color: white;
   padding: 20px;
-  height: 100vh;
+  height: 100%;
 `
 
 export const SettingsContainer = styled.div`

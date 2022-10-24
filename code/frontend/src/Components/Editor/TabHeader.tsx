757afbedf0f78f405367508207020d77ddb218e9
Samuel Truman <samuel.truman@uni-wuerzburg.de>
2022-10-24 15:22:31 +0200

    Consistent naming

--- code/frontend/src/Components/Editor/TabHeader.tsx
+++ code/frontend/src/Components/Editor/TabHeader.tsx
@@ -4,8 +4,8 @@ export const TabHeader = ({setId}) => {
     return <div style={{textAlign: 'center', backgroundColor: '#15171b'}}>
         <div style={{padding: 5, display: 'inline-block'}}>
             <Button onClick={() => setId(1)}>Objects</Button>
-            <Button onClick={() => setId(2)}>Behavior</Button>
-            <Button onClick={() => setId(3)}>Avatar</Button>
+            <Button onClick={() => setId(2)}>Behaviors</Button>
+            <Button onClick={() => setId(3)}>Avatars</Button>
             <Button onClick={() => setId(4)}>Articy</Button>
             <Button onClick={() => setId(5)}>Share</Button>
         </div>

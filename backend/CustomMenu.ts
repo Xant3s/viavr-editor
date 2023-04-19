c6c9095c6f5d5e2bc298fb3f2b8f8923aaecfb82
Samuel Truman <samuel.truman@uni-wuerzburg.de>
2023-04-19 14:42:07 +0200

    Refactoring

--- backend/CustomMenu.ts
+++ backend/CustomMenu.ts
@@ -1,6 +1,8 @@
 import { app, ipcMain as ipc, Menu, shell } from 'electron'
 
 export default class CustomMenu {
+    private projectLoaded = false
+
     public loadCustomMenu() {
         const menu = Menu.buildFromTemplate([
             {
@@ -10,13 +12,13 @@ export default class CustomMenu {
                         label: 'Save Current Scene',
                         click: () => ipc.emit('save-current-scene'),
                         id: 'SaveScene',
-                        enabled: false,
+                        enabled: this.projectLoaded,
                     },
                     {
                         label: 'Save Project',
                         click: () => ipc.emit('project-manager:save-project'),
                         id: 'SaveProject',
-                        enabled: false,
+                        enabled: this.projectLoaded,
                     },
                     {
                         label: 'Project Settings',
@@ -39,7 +41,7 @@ export default class CustomMenu {
                         label: 'Generate VIA Experience',
                         click: () => ipc.emit('BuildSystem:open-build-menu'),
                         id: 'GenerateExperience',
-                        enabled: false,
+                        enabled: this.projectLoaded,
                     },
                 ],
             },
@@ -69,7 +71,7 @@ export default class CustomMenu {
                         label: 'Open present working directory',
                         click: () => ipc.emit('dev:open-pwd'),
                         id: 'OpenDirectory',
-                        enabled: false,
+                        enabled: this.projectLoaded,
                     },
                     {
                         role: 'toggleDevTools',
@@ -80,15 +82,8 @@ export default class CustomMenu {
         Menu.setApplicationMenu(menu)
     }
 
-    public unlockMenuOptionsUponProjectOpened(){
-        const saveProject = Menu.getApplicationMenu()?.getMenuItemById("SaveProject")
-        const saveScene = Menu.getApplicationMenu()?.getMenuItemById("SaveScene")
-        const generateExperience =  Menu.getApplicationMenu()?.getMenuItemById("GenerateExperience")
-        const openDirectory = Menu.getApplicationMenu()?.getMenuItemById("OpenDirectory")
-
-        if(saveProject) saveProject.enabled = true
-        if(saveScene) saveScene.enabled = true
-        if(generateExperience) generateExperience.enabled = true
-        if(openDirectory) openDirectory.enabled = true
+    public unlockMenuOptionsUponProjectOpened() {
+        this.projectLoaded = true
+        this.loadCustomMenu()
     }
 }

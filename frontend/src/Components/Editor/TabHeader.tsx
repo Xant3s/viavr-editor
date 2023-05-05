2d152d8b3af56e4854631a86aa082504b7a3db9d
Lea Kohl <lea.kohl@stud-mail.uni-wuerzburg.de>
2023-05-05 13:42:10 +0200

    Buttons now stay highlited until one has clicked elsewhere

--- frontend/src/Components/Editor/TabHeader.tsx
+++ frontend/src/Components/Editor/TabHeader.tsx
@@ -1,13 +1,13 @@
-import { Button } from '../StyledComponents/Button'
+import { TabButton } from '../StyledComponents/TabButton'
 
 export const TabHeader = ({ setId, hidden = false }) => {
     return <div hidden={hidden} style={{ textAlign: 'center', backgroundColor: '#15171b' }}>
         <div style={{ padding: 5, display: 'inline-block' }}>
-            <Button onClick={() => setId(1)}>Objects</Button>
-            <Button onClick={() => setId(2)}>Behaviors</Button>
-            <Button onClick={() => setId(3)}>Avatars</Button>
-            <Button onClick={() => setId(4)}>Articy</Button>
-            <Button onClick={() => setId(5)}>Share</Button>
+            <TabButton onClick={() => setId(1)}>Objects</TabButton>
+            <TabButton onClick={() => setId(2)}>Behaviors</TabButton>
+            <TabButton onClick={() => setId(3)}>Avatars</TabButton>
+            <TabButton onClick={() => setId(4)}>Articy</TabButton>
+            <TabButton onClick={() => setId(5)}>Share</TabButton>
         </div>
     </div>
 }

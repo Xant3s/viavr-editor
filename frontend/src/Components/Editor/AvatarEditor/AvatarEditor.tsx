238e3a8b490d488e27126a141d8f2fc0ce0e81df
Samuel Truman <samuel.truman@uni-wuerzburg.de>
2023-05-31 14:01:56 +0200

    Save settings to project and build settings, load from project settings

--- frontend/src/Components/Editor/AvatarEditor/AvatarEditor.tsx
+++ frontend/src/Components/Editor/AvatarEditor/AvatarEditor.tsx
@@ -17,29 +17,6 @@ export const AvatarEditor = ({ hidden }) => {
     const [sceneObjects, setSceneObjects] = React.useState<any[]>([])
 
 
-    const loadSceneObjects = async () => {
-        const objects = await api.invoke(api.channels.toMain.getSceneObjects)
-        setSceneObjects(objects)
-    }
-
-    useEffect(() => {
-        if(!hidden) loadSceneObjects()
-    }, [hidden])
-
-    useEffect(() => {
-        const loadAvatars = async () => {
-            // TODO: load all settings from project settings
-            const projectSettings = await api.invoke(api.channels.toMain.requestProjectSettings)
-            const avatarsSetting = projectSettings.find(([k, _]) => k === 'dev.avatars')
-            const avatars = avatarsSetting[1].value || []
-            setAvatarsSettingUuid(avatarsSetting[1].uuid || '')
-            setAvatars(avatars)
-            // TODO: check if avatars are downloaded
-        }
-
-        api.on(api.channels.fromMain.projectOpened, loadAvatars)
-    }, [])
-
     const updateQrCode = (avatarId) => {
         const token = avatars.find(avatar => avatar.id === avatarId)?.token || ''
         QrToString(token, { type: 'svg' }, (err, svg) => {
@@ -53,16 +30,15 @@ export const AvatarEditor = ({ hidden }) => {
         const newAvatar = { name: name, id: uuid, token: token, articyId: '', sceneObject: '' }
         const newAvatars = [...avatars, newAvatar]
         setAvatars(newAvatars)
-        api.send(api.channels.toMain.changeProjectSetting, avatarsSettingUuid, newAvatars)
-        // TODO: save all settings to project settings
-        // TODO: export to build settings
+        saveAll()
     }
 
     const deleteAvatar = (avatarId) => {
         const newAvatars = avatars.filter(avatar => avatar.id !== avatarId)
         setAvatars(newAvatars)
-        api.send(api.channels.toMain.changeProjectSetting, avatarsSettingUuid, newAvatars)
         // Todo: delete downloaded avatar from project files
+        // TODO: send delete request to TUD server?
+        saveAll()
     }
 
     const assignSceneObject = (avatarId, sceneObjectId) => {
@@ -73,7 +49,35 @@ export const AvatarEditor = ({ hidden }) => {
             return avatar
         })
         setAvatars(newAvatars)
+        saveAll()
     }
+
+    const saveAll = () => {
+        api.send(api.channels.toMain.changeProjectSetting, avatarsSettingUuid, avatars)
+        api.invoke(api.channels.toMain.setBuildSetting, 'avatars', avatars)
+    }
+
+    const loadSceneObjects = async () => {
+        const objects = await api.invoke(api.channels.toMain.getSceneObjects)
+        setSceneObjects(objects)
+    }
+
+    useEffect(() => {
+        if(!hidden) loadSceneObjects()
+    }, [hidden])
+
+    useEffect(() => {
+        const loadAvatars = async () => {
+            const projectSettings = await api.invoke(api.channels.toMain.requestProjectSettings)
+            const avatarsSetting = projectSettings.find(([k, _]) => k === 'dev.avatars')
+            const avatars = avatarsSetting[1].value || []
+            setAvatarsSettingUuid(avatarsSetting[1].uuid || uuid4())
+            setAvatars(avatars)
+            // TODO: check if avatars are downloaded
+        }
+
+        api.on(api.channels.fromMain.projectOpened, loadAvatars)
+    }, [])
 
 
     return <AvatarEditorContainer hidden={hidden}>

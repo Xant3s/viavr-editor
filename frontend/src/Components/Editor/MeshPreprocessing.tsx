fc96e48a229b737adc49787584de2092bef9f39e
Samuel Truman <samuel.truman@uni-wuerzburg.de>
2023-06-15 14:38:09 +0200

    Added UI

--- frontend/src/Components/Editor/MeshPreprocessing.tsx
+++ frontend/src/Components/Editor/MeshPreprocessing.tsx
@@ -1,5 +1,24 @@
+import { FileCard, FileUploader, MimeType, Pane, toaster } from 'evergreen-ui'
+import { useCallback, useState } from 'react'
+import { Button } from '../StyledComponents/Button'
 
 export const MeshPreprocessing = ({ hidden }) => {
+    const [files, setFiles] = useState<any[]>([])
+    const [fileRejections, setFileRejections] = useState<any[]>([])
+
+    const handleChange = useCallback((files) => setFiles([files[0]]), [])
+    const handleRejected = useCallback((fileRejections) => setFileRejections([fileRejections[0]]), [])
+    const handleRemove = useCallback(() => {
+        setFiles([])
+        setFileRejections([])
+    }, [])
+
+
+    function runPreprocessor(e) {
+        e.preventDefault()
+        toaster.notify('not implemented yet')
+    }
+
     return <div
         hidden={hidden}
         style={{
@@ -12,6 +31,40 @@ export const MeshPreprocessing = ({ hidden }) => {
         }}
     >
         <h1>Optimize 3D Objects</h1>
-        <div></div>
+        {/*'model/gltf+json '*/}
+        <form onSubmit={runPreprocessor}>
+            <div>TODO: info how to use</div>
+            <Pane maxWidth={654}>
+                <FileUploader
+                    maxFiles={1}
+                    onChange={handleChange}
+                    onRejected={handleRejected}
+                    renderFile={(file) => {
+                        const { name, size, type } = file
+                        const fileRejection = fileRejections.find((fileRejection) => fileRejection.file === file)
+                        const { message } = fileRejection || {}
+                        return (
+                            <FileCard
+                                key={name}
+                                isInvalid={fileRejection != null}
+                                name={name}
+                                onRemove={handleRemove}
+                                sizeInBytes={size}
+                                type={type}
+                                validationMessage={message}
+                            />
+                        )
+                    }}
+                    values={files}
+                />
+            </Pane>
+
+            <details>
+                <summary>Advanced Settings</summary>
+                Test2
+            {/*    percent, use textures, output file, use buffers*/}
+            </details>
+            <Button type='submit' disabled={files.length === 0}>Optimize</Button>
+        </form>
     </div>
 }

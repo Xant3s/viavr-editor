3920cbd3b233a234db7a96c055f5cd6ef4b2b7b6
Samuel Truman <samuel.truman@uni-wuerzburg.de>
2023-06-19 16:31:05 +0200

    Added all settings (UI only)

--- frontend/src/Components/Editor/MeshPreprocessing.tsx
+++ frontend/src/Components/Editor/MeshPreprocessing.tsx
@@ -20,6 +20,11 @@ export const MeshPreprocessing = ({ hidden }) => {
     const [percentValue, setPercentValue] = useState<number>(50)
     const [embedTextures, setEmbedTextures] = useState<boolean>(true)
     const [embedBuffers, setEmbedBuffers] = useState<boolean>(true)
+    const [noNormalMaps, setNoNormalMaps] = useState<boolean>(false)
+    const [adjustExistingNormalMaps, setAdjustExistingNormalMaps] = useState<boolean>(false)
+    const [useVertexNormals, setUseVertexNormals] = useState<boolean>(false)
+    const [creaseAngle, setCreaseAngle] = useState<number>(70)
+    const [normalDeviation, setNormalDeviation] = useState<number>(10)
     const [files, setFiles] = useState<any[]>([])
     const [fileRejections, setFileRejections] = useState<any[]>([])
 
@@ -121,8 +126,7 @@ export const MeshPreprocessing = ({ hidden }) => {
 
             <Pane minWidth={500} maxWidth={654} marginBottom={25}>
                 <SettingAccordion summary={'Advanced Settings'}  details={
-                    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                        <div>
+                    <div>
                         <Pane maxWidth={200}>
                             <Typography id="non-linear-slider" gutterBottom style={{textAlign: 'left'}}>
                                 Target Percentage: {percentValue.toString()}
@@ -139,7 +143,6 @@ export const MeshPreprocessing = ({ hidden }) => {
                                 aria-labelledby="non-linear-slider"
                             />
                         </Pane>
-
                         <Label htmlFor="embedTexturesSwitch" marginBottom={8} display="flex" alignItems="left" justifyContent="left" color="white">
                             <Switch
                                 id="embedTexturesSwitch"
@@ -147,7 +150,7 @@ export const MeshPreprocessing = ({ hidden }) => {
                                 onChange={(e) => setEmbedTextures(e.target.checked)}
                                 marginRight={8}
                             />
-                            <span>Embed Textures</span>
+                            <span>Embed textures</span>
                         </Label>
                         <Label htmlFor="embedBuffersSwitch" marginBottom={8} display="flex" alignItems="left" justifyContent="left" color="white">
                             <Switch
@@ -156,9 +159,67 @@ export const MeshPreprocessing = ({ hidden }) => {
                                 onChange={(e) => setEmbedBuffers(e.target.checked)}
                                 marginRight={8}
                             />
-                            <span>Embed Buffers</span>
+                            <span>Embed buffers</span>
                         </Label>
-                        </div>
+                        <Label htmlFor="noNormalMapsSwitch" marginBottom={8} display="flex" alignItems="left" justifyContent="left" color="white">
+                            <Switch
+                                id="noNormalMapsSwitch"
+                                checked={noNormalMaps}
+                                onChange={(e) => setNoNormalMaps(e.target.checked)}
+                                marginRight={8}
+                            />
+                            <span>Don&apos;t generate normal map</span>
+                        </Label>
+                        <Label htmlFor="adjustExistingNormalMapsSwitch" marginBottom={8} display="flex" alignItems="left" justifyContent="left" color="white">
+                            <Switch
+                                id="adjustExistingNormalMapsSwitch"
+                                checked={adjustExistingNormalMaps}
+                                onChange={(e) => setAdjustExistingNormalMaps(e.target.checked)}
+                                marginRight={8}
+                            />
+                            <span>Adjust existing normal maps</span>
+                        </Label>
+                        <Label htmlFor="useVertexNormalsSwitch" marginBottom={8} display="flex" alignItems="left" justifyContent="left" color="white">
+                            <Switch
+                                id="useVertexNormalsSwitch"
+                                checked={useVertexNormals}
+                                onChange={(e) => setUseVertexNormals(e.target.checked)}
+                                marginRight={8}
+                            />
+                            <span>Use vertex normals</span>
+                        </Label>
+                        <Pane maxWidth={200}>
+                            <Typography id="non-linear-slider" gutterBottom style={{textAlign: 'left'}}>
+                                Crease angle: {creaseAngle.toString()}
+                            </Typography>
+                            <Slider
+                                value={creaseAngle}
+                                min={1}
+                                step={1}
+                                max={150}
+                                getAriaValueText={() => creaseAngle.toString()}
+                                valueLabelFormat={() => creaseAngle.toString()}
+                                onChange={(e, newValue) => setCreaseAngle(newValue as number)}
+                                valueLabelDisplay="auto"
+                                aria-labelledby="non-linear-slider"
+                            />
+                        </Pane>
+                        <Pane maxWidth={200}>
+                            <Typography id="non-linear-slider" gutterBottom style={{textAlign: 'left'}}>
+                                Normal deviation: {normalDeviation.toString()}
+                            </Typography>
+                            <Slider
+                                value={normalDeviation}
+                                min={1}
+                                step={1}
+                                max={100}
+                                getAriaValueText={() => normalDeviation.toString()}
+                                valueLabelFormat={() => normalDeviation.toString()}
+                                onChange={(e, newValue) => setNormalDeviation(newValue as number)}
+                                valueLabelDisplay="auto"
+                                aria-labelledby="non-linear-slider"
+                            />
+                        </Pane>
                     </div>
                 } />
             </Pane>

6efcabc783207954a47966f0047e725aa1673c75
Lea Kohl <lea.kohl@stud-mail.uni-wuerzburg.de>
2023-08-17 10:06:39 +0200

    Automatically select scene circumventing load scene page

--- frontend/src/SpokeEditor/SceneLoadingPage.ts
+++ frontend/src/SpokeEditor/SceneLoadingPage.ts
@@ -5,27 +5,19 @@ import { SpokeAPI } from './SpokeAPI'
 export default class SceneLoadingPage {
 
     constructor() {
-        SpokeAPI.Instance.addEventListener(SpokeAPI.Messages.fromSpoke.projectPageSelected, this.displayAvailableScenes.bind(this))
-        this.displayAvailableScenes()
+        this.createOrLoadScene()
     }
 
-    private async displayAvailableScenes() {
+    private async createOrLoadScene(){
+        const availableScenes: string[] = await api.invoke(api.channels.toMain.queryJsonScenes)
+        if(availableScenes.length > 0){
+            await this.loadScene()
+            return
+        }
         const newSceneLabel = $$('h3:contains("New Scene")')
         const sceneLoadButton = newSceneLabel.parent()
-        const availableScenes: string[] = await api.invoke(api.channels.toMain.queryJsonScenes)
-        availableScenes.forEach(sceneName => this.addLoadSceneButton(sceneName, sceneLoadButton))
+        if (sceneLoadButton.length > 0) sceneLoadButton.trigger("click")
     }
-
-    private addLoadSceneButton(sceneName: string, newSceneButton: JQuery<HTMLElement>) {
-        const sceneNameWithoutExtension = api.Path.parse(sceneName).name
-        const newButton = newSceneButton.clone()
-        newButton.find('svg').hide()
-        newButton.find('h3').text(sceneNameWithoutExtension)
-        newButton.attr('href', '#')
-        newButton.on('click', async () => this.loadScene())
-        newButton.insertAfter(newSceneButton)
-    }
-
     private async loadScene() {
         const sceneFileContents = await api.invoke(api.channels.toMain.getSceneFileContents)
         if(sceneFileContents === '') {

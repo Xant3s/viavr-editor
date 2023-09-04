d14ade7d79d217f0d29daae345879052557e74f1
Samuel Truman <samuel.truman@uni-wuerzburg.de>
2023-09-04 13:41:36 +0200

    Added functionality to extract downloaded avatars

--- backend/AvatarManager.ts
+++ backend/AvatarManager.ts
@@ -6,35 +6,68 @@ import PreferencesManager from './Preferences/PreferencesManager'
 import fetch from 'node-fetch'
 import fs from 'fs'
 import { StringSetting } from '../frontend/src/@types/Settings'
+import Utils from './BuildSystem/Utils'
+
 
 export class AvatarManager {
+    private presentWorkingDirectory: string | undefined
+    private avatarServerUrl: string | undefined
+    
+
     public constructor() {
-        ProjectManager.getInstance().registerOnProjectLoadedListener(() => {
-            FileUtils.ensurePathExists(ProjectManager.getInstance().presentWorkingDirectory + '/Avatars')
+        ProjectManager.getInstance().registerOnProjectLoadedListener(async () => {
+            this.presentWorkingDirectory = ProjectManager.getInstance().presentWorkingDirectory
+            const avatarServerUrlSetting: StringSetting = await PreferencesManager.getInstance().get('avatarServer')
+            this.avatarServerUrl = avatarServerUrlSetting.value
+            FileUtils.ensurePathExists(this.presentWorkingDirectory + '/Avatars')
         })
         
-        ipcMain.handle(channels.toMain.downloadAvatar, async (event, args) => {
-            try {
-                const avatarServerUrlSetting: StringSetting = await PreferencesManager.getInstance().get('avatarServer')
-                const avatarServerUrl = avatarServerUrlSetting.value
-                const destination = `${ProjectManager.getInstance().presentWorkingDirectory}/Avatars/${args}.zip`
-                const response = await fetch(`${avatarServerUrl}/scans/download`, {
-                    headers: {
-                        'x-scan-id': args
-                    }
-                })
-                const buffer = await response.arrayBuffer()
-                await fs.promises.writeFile(destination, Buffer.from(buffer))
-            } catch(e) {
-                console.log(e)
-                return 1
-            }
+        ipcMain.handle(channels.toMain.downloadAvatar, async (event, avatarToken) => {
+            if(!this.isReady()) return 2
+            const errorCode = await this.downloadAvatar(avatarToken)
+            if(errorCode !== 0) return errorCode
+            await this.extractAvatar(avatarToken)
+            await this.deleteZip(avatarToken)
             return 0
         })
         
         ipcMain.handle(channels.toMain.deleteAvatarFromFileSystem, async (event, args) => {
-            const path = `${ProjectManager.getInstance().presentWorkingDirectory}/Avatars/${args}.zip`
+            if(!this.isReady()) return 2
+            const path = `${this.presentWorkingDirectory}/Avatars/${args}.zip`
             await fs.promises.rm(path)
         })
+    }
+
+    private async downloadAvatar(avatarToken: string) {
+        try {
+            const destination = `${this.presentWorkingDirectory}/Avatars/${avatarToken}.zip`
+            const response = await fetch(`${this.avatarServerUrl}/scans/download`, {
+                headers: {
+                    'x-scan-id': avatarToken
+                }
+            })
+            const buffer = await response.arrayBuffer()
+            await fs.promises.writeFile(destination, Buffer.from(buffer))
+        } catch(e) {
+            console.log(e)
+            return 1
+        }
+        return 0
+    }
+
+    private async extractAvatar(avatarToken: string) {
+        const zipPath = `${this.presentWorkingDirectory}/Avatars/${avatarToken}.zip`
+        const destination = `${this.presentWorkingDirectory}/Avatars/${avatarToken}`
+        FileUtils.ensurePathExists(destination)
+        await Utils.extractZipToPath(zipPath, destination)
+    }
+    
+    private async deleteZip(avatarToken: string) {
+        const zipPath = `${this.presentWorkingDirectory}/Avatars/${avatarToken}.zip`
+        await fs.promises.rm(zipPath)
+    }
+
+    private isReady() {
+        return this.presentWorkingDirectory !== undefined && this.avatarServerUrl !== undefined
     }
 }
--- docs/specifications/0.1.0/Avatars/Avatars.md
+++ docs/specifications/0.1.0/Avatars/Avatars.md
@@ -2,7 +2,7 @@
 
 ## Avatars in Unity
 
-- When the Unity project is created all downloaded avatars are exported to `Assets/Avatars` (currently `Assets/Avatars/uuid.zip`, I would like this to be `Assets/Avatars/uuid.fbx`)
+- When the Unity project is created all downloaded avatars are exported to `Assets/Avatars` (currently `Assets/Avatars/uuid/result.fbx` and `Assets/Avatars/uuid/skin_basecolor.png`)
 - The list of available avatars is stored in the build settings
   - During edit time (when the avatar loader package should be doing its thing) the build settings are available at `Assets/Settings/BuildSettings.json`
   - During runtime the build settings are available at `Resources/BuildSettings.json`
@@ -26,5 +26,5 @@ Example build settings:
 }
 ```
 
-- `token` is the TUD server ID. This is also the file name, e.g. `Assets/Avatars/7972979e-4710-11ee-82ac-d6007ec8b6a7.zip`
+- `token` is the TUD server ID. This is also the folder name, e.g. `Assets/Avatars/7972979e-4710-11ee-82ac-d6007ec8b6a7/result.fbx`
 - `sceneObject` is the uuid for the corresponding placeholder object in the scene. You can find that GameObject using its `Uuid` component. The `sceneObject` value corresponds to `Uuid.uuid` (the public variable of type `Guid` named `uuid` in the `Uuid` `MonoBehaviour` component)

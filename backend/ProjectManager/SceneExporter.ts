8ffa8e8a7e2b3449ced8a3ce5e4bb1f266a745a3
Samuel Truman <samuel.truman@uni-wuerzburg.de>
2023-09-07 17:32:46 +0200

    Added workaround for duplicated .spoke export

--- backend/ProjectManager/SceneExporter.ts
+++ backend/ProjectManager/SceneExporter.ts
@@ -6,6 +6,7 @@ import { API, channels } from '../API'
 
 export default class SceneExporter {
     private mainWindow: MainWindow
+    private startedToExportSpoke = false
     private exportedSpoke = false
     private exportedGlb = false
 
@@ -19,6 +20,7 @@ export default class SceneExporter {
 
     public async exportScene(): Promise<void> {
         return new Promise((resolve, reject) => {
+            this.startedToExportSpoke = false
             this.exportedSpoke = false
             this.exportedGlb = false
             this.mainWindow.send(channels.fromMain.spokeExportScene)
@@ -38,6 +40,11 @@ export default class SceneExporter {
     private setSaveScenePathToProjectFolder() {
         session.defaultSession.on('will-download', async (event, item, webContents) => {
             if (!item.getFilename().endsWith('.glb') && !item.getFilename().endsWith('.spoke')) return
+            if(item.getFilename().endsWith('.spoke') && this.startedToExportSpoke) {
+                item.cancel()
+                return
+            }
+            this.startedToExportSpoke = true
             const saveScenePath = Path.join(
                 ProjectManager.getInstance().presentWorkingDirectory,
                 'Scenes',

6ea5b1d52de89599abc982f638c290512e88a6aa
Samuel Truman <samuel.truman@uni-wuerzburg.de>
2023-10-13 16:24:33 +0200

    Fixed unhandled exceptions when fetching packages fails

--- backend/BuildSystem/UnityPackageManager.ts
+++ backend/BuildSystem/UnityPackageManager.ts
@@ -3,6 +3,7 @@ import fetch from 'node-fetch'
 import { channels } from '../API'
 import PreferencesManager from '../Preferences/PreferencesManager'
 import { PackageRegistries } from './DataStructures/PackageRegistries'
+import { Logger } from '../Logger'
 
 
 export default class UnityPackageManager {
@@ -37,21 +38,33 @@ export default class UnityPackageManager {
     }
 
     public async queryPackagesFromRegistry(registryUrl: string, scope: string, filterViavr = true) {
-        const response = await fetch(`${registryUrl}/-/v1/search?text=${scope}`)
-        const packageList = await response.json()
-        // @ts-ignore
-        const packages = packageList['objects'].map(obj => obj['package'])
-        const packageDetails = await Promise.all(packages.map(p => this.queryPackageDetails(registryUrl, p['name'])))
-        let packagesLatestInfo = await Promise.all(packageDetails.map(packageInfo => UnityPackageManager.getLatestPackageVersion(packageInfo)))
-        if(filterViavr) {
-            packagesLatestInfo = packagesLatestInfo.filter(p => p.keywords && (p.keywords as string[]).indexOf('viavr') !== -1)
+        try{
+            const response = await fetch(`${registryUrl}/-/v1/search?text=${scope}`)
+            const packageList = await response.json()
+            // @ts-ignore
+            const packages = packageList['objects'].map(obj => obj['package'])
+            let packageDetails = await Promise.all(packages.map(p => this.queryPackageDetails(registryUrl, p['name'])))
+            packageDetails = packageDetails.filter(p => p !== {})
+            if(packageDetails.length === 0) return []
+            let packagesLatestInfo = await Promise.all(packageDetails.map(packageInfo => UnityPackageManager.getLatestPackageVersion(packageInfo)))
+            if(filterViavr) {
+                packagesLatestInfo = packagesLatestInfo.filter(p => p.keywords && (p.keywords as string[]).indexOf('viavr') !== -1)
+            }
+            return packagesLatestInfo
+        } catch(e) {
+            Logger.get().log('Failed to query packages from registry: ' + registryUrl + ' with scope: ' + scope + e as string)
+            return []
         }
-        return packagesLatestInfo
     }
 
     public async queryPackageDetails(registryUrl: string, packageName: string) {
-        const res = await fetch(`${registryUrl}/${packageName}`)
-        return await res.json()
+        try{
+            const res = await fetch(`${registryUrl}/${packageName}`)
+            return await res.json() 
+        } catch(e) {
+            Logger.get().log('Failed to query package details from registry: ' + registryUrl + ' with package name: ' + packageName + e as string)
+            return {}
+        }
     }
 
     public static async getLatestPackageVersion(packageInfo) {

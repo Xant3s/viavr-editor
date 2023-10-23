d9a9a1f9e99d19944bbc77fe87ed4f9ee7286e13
Johannes Büttner <johannes.buettner@uni-wuerzburg.de>
2023-10-23 13:27:32 +0200

    Change drawIO language to English

--- frontend/src/Components/FloorMapEditor/DrawioEditor.tsx
+++ frontend/src/Components/FloorMapEditor/DrawioEditor.tsx
@@ -10,7 +10,7 @@ export const DrawioEditor = ({onFloorMapAvailable}) => {
 
 
     const openDrawio = (evt) => {
-        const url = 'https://embed.diagrams.net/?embed=1&ui=dark&spin=1&modified=unsavedChanges&proto=json'
+        const url = 'https://embed.diagrams.net/?embed=1&ui=dark&spin=1&modified=unsavedChanges&proto=json&lang=en'
         const source = evt.srcElement || evt.target
 
         if(source.nodeName === 'IMG' && source.className === 'drawio') {

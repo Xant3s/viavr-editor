c351c96dec069f940b35c94262a622defa75bab9
Samuel Truman <samuel.truman@uni-wuerzburg.de>
2023-10-25 11:51:11 +0200

    Updated tab tooltip texts and color

--- frontend/src/Components/Editor/TabHeader.tsx
+++ frontend/src/Components/Editor/TabHeader.tsx
@@ -11,6 +11,9 @@ interface props {
     returnToWelcomeScreen: () => void
 }
 
+const tooltipIconStyle = { marginLeft: 10, fontSize: 14 }
+
+
 export const TabHeader = ({ setId, hidden, isInTutorialMode, returnToWelcomeScreen}) => {
     const [lastClicked, setLastClicked] = useState<number>(1)
     const [isDisabled, setDisabled] = useState(false)
@@ -35,37 +38,37 @@ export const TabHeader = ({ setId, hidden, isInTutorialMode, returnToWelcomeScre
                     : <>
                         <TabButton disabled={isDisabled} onClick={() => handleClick(6)} style={lastClicked === 6 ? { background: '#3A4048', color: '#FFFFFF' } : {}}>
                             Optimize
-                            <HelpOutlineIcon data-tooltip-id="Optimize" data-tooltip-content="This tab allows for the optimization of 3D objects in the .gltf file format." style={{ color: '#4D535B', marginLeft: 10,fontSize: 14 }}/>
+                            <HelpOutlineIcon data-tooltip-id="Optimize" data-tooltip-content="Optimize 3D objects in the .glb file format." style={tooltipIconStyle}/>
                             <Tooltip id="Optimize" place="bottom" style={{fontSize: '14px'}} />
                         </TabButton>
                         <TabButton disabled={isDisabled} onClick={() => handleClick(1)} style={lastClicked === 1 ? { background: '#3A4048', color: '#FFFFFF' } : {}}>
                             Objects
-                            <HelpOutlineIcon data-tooltip-id="Objects" data-tooltip-content="This tab lets allows for the creation and editing of objects in the scene." style={{ color: '#4D535B', marginLeft: 10,fontSize: 14 }}/>
+                            <HelpOutlineIcon data-tooltip-id="Objects" data-tooltip-content="Create and edit objects in the scene." style={tooltipIconStyle}/>
                             <Tooltip id="Objects" place="bottom" style={{fontSize: '14px'}} />
                         </TabButton>
                         <TabButton disabled={isDisabled} onClick={() => handleClick(2)} style={lastClicked === 2 ? { background: '#3A4048', color: '#FFFFFF' } : {}}>
                             Behaviors
-                            <HelpOutlineIcon data-tooltip-id="Behaviors" data-tooltip-content="This tab allows for the creation and editing of meta data, variables and events." style={{ color: '#4D535B', marginLeft: 10,fontSize: 14 }}/>
+                            <HelpOutlineIcon data-tooltip-id="Behaviors" data-tooltip-content="Tag objects and create events." style={tooltipIconStyle}/>
                             <Tooltip id="Behaviors" place="bottom" style={{fontSize: '14px'}} />
                             </TabButton>
                         <TabButton disabled={isDisabled} onClick={() => handleClick(3)} style={lastClicked === 3 ? { background: '#3A4048', color: '#FFFFFF' } : {}}>
                             Avatars
-                            <HelpOutlineIcon data-tooltip-id="Avatars" data-tooltip-content="This tab allows for the creation and download of avatars via the VIA-VR avatar app." style={{ color: '#4D535B', marginLeft: 10,fontSize: 14 }}/>
+                            <HelpOutlineIcon data-tooltip-id="Avatars" data-tooltip-content="Create and download characters via the VIA-VR avatar app." style={tooltipIconStyle}/>
                             <Tooltip id="Avatars" place="bottom" style={{fontSize: '14px'}} />
                             </TabButton>
                         <TabButton disabled={isDisabled} onClick={() => handleClick(4)} style={lastClicked === 4 ? { background: '#3A4048', color: '#FFFFFF' } : {}}>
                             Articy
-                            <HelpOutlineIcon data-tooltip-id="Articy" data-tooltip-content="This tab serves as an interface for articy."  style={{ color: '#4D535B', marginLeft: 10,fontSize: 14 }}/>
+                            <HelpOutlineIcon data-tooltip-id="Articy" data-tooltip-content="Use the Articy editor to create dialogs and assign dialogs to characters."  style={tooltipIconStyle}/>
                             <Tooltip id="Articy" place="bottom" style={{fontSize: '14px'}} />
                             </TabButton>
                         <TabButton disabled={isDisabled} onClick={() => handleClick(5)} style={lastClicked === 5 ? { background: '#3A4048', color: '#FFFFFF' } : {}}>
                             Share
-                            <HelpOutlineIcon data-tooltip-id= "Share"data-tooltip-content="This tab allows you to share the project." style={{ color: '#4D535B', marginLeft: 10,fontSize: 14 }}/>
+                            <HelpOutlineIcon data-tooltip-id= "Share"data-tooltip-content="(Experimental) Share the project." style={tooltipIconStyle}/>
                             <Tooltip id="Share" place="bottom" style={{fontSize: '14px'}} />
                             </TabButton>
                         <TabButton disabled={isDisabled} onClick={() => handleClick(7)} style={lastClicked === 7 ? { background: '#3A4048', color: '#FFFFFF' } : {}}>
                             Finish
-                            <HelpOutlineIcon data-tooltip-id="Finish" data-tooltip-content="This tab is used to export the project, select used packages and create and edit floor maps." style={{ color: '#4D535B', marginLeft: 10,fontSize: 14 }}/>
+                            <HelpOutlineIcon data-tooltip-id="Finish" data-tooltip-content="Create the experience, select and configure additional features." style={tooltipIconStyle}/>
                             <Tooltip id="Finish" place="bottom" style={{fontSize: '14px'}} />
                             </TabButton>
 

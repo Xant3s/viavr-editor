d7561a6832bae3c312288de21002d2a589438e2f
Johannes Büttner <johannes.buettner@uni-wuerzburg.de>
2024-03-15 13:28:10 +0100

    Add missing dependencies to useEffect

--- frontend/src/Components/Editor/BehaviorEditor/ActionComponent.tsx
+++ frontend/src/Components/Editor/BehaviorEditor/ActionComponent.tsx
@@ -42,7 +42,7 @@ const ActionComponent = (props) => {
             setActionButtonText(newAction.displayName)
         }
 
-    }, [])
+    }, [props.component.name, props.component.parameters])
 
     return (
         <SettingAccordionAction

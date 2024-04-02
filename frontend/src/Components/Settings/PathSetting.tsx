c0754524750314972cc5994f766b099b0769a29c
Samuel Truman <samuel.truman@uni-wuerzburg.de>
2024-04-02 15:26:46 +0200

    Improved user feedback when using the path setting

--- frontend/src/Components/Settings/PathSetting.tsx
+++ frontend/src/Components/Settings/PathSetting.tsx
@@ -2,13 +2,20 @@ import { FilePicker } from 'evergreen-ui'
 import { SettingEntryLabel } from '../StyledComponents/Preferences/StyledSettings'
 
 export const PathSetting = ({ id, uuid, label, value, onChange }) => {
-    return <div style={{ display: 'flex', alignItems: 'center' }}>
-        {label && <SettingEntryLabel htmlFor={id}>{label}:</SettingEntryLabel>}
-        <FilePicker id={id}
-                    width={350}
-                    height={24}
-                    onChange={(e) => onChange(uuid, e[0]['path'])}
-                    placeholder={value || 'Select a file'}
-        />
-    </div>
+    return <>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+            {label && <SettingEntryLabel htmlFor={id}>{label}:</SettingEntryLabel>}
+            <FilePicker id={id}
+                        width={350}
+                        height={24}
+                        onChange={(e) => onChange(uuid, e[0]['path'])}
+                        placeholder={value || 'Select a file'}
+            />
+        </div>
+        {value && (
+            <div style={{ marginLeft: '10px' }}>
+                Selected path: <span style={{ fontWeight: 'bold' }}>{value}</span>
+            </div>
+        )}
+    </>
 }

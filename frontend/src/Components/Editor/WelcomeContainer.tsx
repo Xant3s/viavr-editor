177f1440be53f49141051f21a532e80159dcd0c6
Samuel Truman <samuel.truman@uni-wuerzburg.de>
2024-04-08 13:57:08 +0200

    Removed disclaimer from welcome screen

--- frontend/src/Components/Editor/WelcomeContainer.tsx
+++ frontend/src/Components/Editor/WelcomeContainer.tsx
@@ -1,5 +1,5 @@
 import { WelcomeContainerStyle } from '../StyledComponents/Editor/StyledEditor'
-import { Alert, toaster } from 'evergreen-ui'
+import { toaster } from 'evergreen-ui'
 import { Row } from '../StyledComponents/Row'
 import { Button } from '../StyledComponents/Button'
 
@@ -20,17 +20,6 @@ export const WelcomeContainer = ({ hidden, startTutorial }) => {
 
     return (
         <WelcomeContainerStyle hidden={hidden}>
-            <Alert
-                intent="warning"
-                title="Before you begin, please be aware:"
-                marginBottom={32}
-                style={{maxWidth: '50%'}}
-            >
-                Please be aware that this is a functional research prototype and not a finalized product.
-                It is not designed for clinical use or to provide medical advice, diagnosis, or treatment. 
-                The application is intended for demonstration and evaluation purposes only, 
-                any use of it is strictly at your own risk. 
-            </Alert>
             <h1>Welcome</h1>
             <div style={{textAlign: 'center', marginBottom: '10px'}}>
                 If you&apos;re new here we recommend going through the tutorial.<br />

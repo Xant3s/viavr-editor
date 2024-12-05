488f864539381dd49cbd514624b0ccb2360946d6
Samuel Truman <samuel.truman@uni-wuerzburg.de>
2024-12-05 16:58:40 +0100

    Simplified SpokeManager. Spoke is no longer invisible but minimized

--- backend/SpokeManager.ts
+++ backend/SpokeManager.ts
@@ -3,14 +3,10 @@ import * as child_process from 'child_process'
 import kill from 'tree-kill'
 import AppUtils from './Utils/AppUtils'
 import PreferencesManager from './Preferences/PreferencesManager'
-import { Logger } from './Logger'
-import find from 'find-process'
 
 export default class SpokeManager {
     private static instance: SpokeManager
-    private startCommand = `yarn start`
-    private port = 9090
-    private pids: number[] = []
+    private spokeProcess : child_process.ChildProcess | undefined = undefined
 
 
     public static getInstance(): SpokeManager {
@@ -26,46 +22,19 @@ export default class SpokeManager {
     }
 
     private async startSpoke() {
-        const foundPids = await this.tryGetSpokePid(this.port)
-        this.pids = [...this.pids, ...foundPids]
-        
-        const psCommand = `powershell -Command "Start-Process -FilePath 'cmd.exe' -ArgumentList '/c ${this.startCommand.replace(/"/g, '`"')}' -WindowStyle Hidden"`
-        const powerShellProcess = child_process.spawn(psCommand, {
+        const psCommand = `powershell -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden yarn start`
+        this.spokeProcess = child_process.spawn(psCommand, {
             shell: true,
             detached: true,
             cwd: `${AppUtils.getResPath()}plugins/Spoke`
         })
-
-        await this.sleep(5_000)  // Workaround. tryGetSpokePid in stopSpoke doesn't seem to work.
-        const newPids = await this.tryGetSpokePid(this.port)
-        this.pids = [...this.pids, ...newPids]
     }
 
     private async stopSpoke() {
         const shouldStop: boolean = await PreferencesManager.getInstance().get<boolean>('dev.stopSpoke') as boolean
 
         if(app.isPackaged || shouldStop) {
-            this.pids.forEach(pid => kill(pid))
+            if(this.spokeProcess && this.spokeProcess.pid) kill(this.spokeProcess.pid)
         }
-    }
-
-    private async tryGetSpokePid(port = 9090) {
-        try {
-            const processes = await find('port', port)
-            if(processes.length === 0) return []
-
-            processes.forEach(p => {
-                Logger.get().logVerbose(`Process using port ${port}: PID=${p.pid}, Name=${p.name}`)
-            })
-
-            return processes.map(p => p.pid)
-        } catch(error) {
-            Logger.get().logVerbose(`Error finding process by port: ${error}`)
-            return []
-        }
-    }
-
-    private sleep(timeInMs: number) {
-        return new Promise<void>((resolve) => setTimeout(() => resolve(), timeInMs))
     }
 }

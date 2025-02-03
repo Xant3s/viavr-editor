bd479f542c3bfca36d58e959af0cc2fb483e0a71
Samuel Truman <samuel.truman@uni-wuerzburg.de>
2025-02-03 17:59:54 +0100

    Added translation for loading screen

--- frontend/src/Components/Utils/CenteredSpinner.tsx
+++ frontend/src/Components/Utils/CenteredSpinner.tsx
@@ -1,13 +1,16 @@
 import { Spinner } from 'evergreen-ui'
+import { useTranslation } from '../../LocalizationContext'
 
 export const CenteredSpinner = ({size = 64, showLabel = true}) => {
+    const {translate} = useTranslation()
+   
     return <div style={{
         width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center',
         flexDirection: 'column', color: 'white',
     }}>
         <Spinner size={size} />
         {showLabel &&
-            <div style={{ fontSize: '24px', marginTop: '16px' }}>Loading...</div>
+            <div style={{ fontSize: '24px', marginTop: '16px' }}>{translate('loading')}</div>
         }
     </div>
 }

2caa1bc71923071aa1f13bd4fe7ab60184901636
Samuel Truman <samuel.truman@uni-wuerzburg.de>
2025-02-20 10:41:33 +0100

    Fixed warning in styled components for remove button icon

--- frontend/src/Components/StyledComponents/RemoveButton.ts
+++ frontend/src/Components/StyledComponents/RemoveButton.ts
@@ -1,9 +1,10 @@
 import styled from 'styled-components'
 import { FaTimes } from 'react-icons/fa'
+import { IconBaseProps } from 'react-icons'
 
-export const RemoveButton = styled(FaTimes)`
-  color: white;
-  cursor: pointer;
-  margin-left: 20px;
-  font-size: 1.5em;
+export const RemoveButton = styled(FaTimes as React.ComponentType<IconBaseProps>)`
+    color: white;
+    cursor: pointer;
+    margin-left: 20px;
+    font-size: 1.5em;
 `
